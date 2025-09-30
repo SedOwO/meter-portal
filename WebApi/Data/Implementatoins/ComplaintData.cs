1e3189d4f6d292b1483f22e0e71cc7644a7a5270
Shashank G N <shashank21naik@gmail.com>
2025-09-30 15:00:18 +0530

    complaint progress

---
+++ WebApi/Data/Implementatoins/ComplaintData.cs
@@ -0,0 +1,47 @@
+﻿using WebApi.Data.Interfaces;
+using WebApi.Models.DB;
+using WebApi.Models.Request;
+using WebApi.Utilities.Interfaces;
+
+namespace WebApi.Data.Implementatoins
+{
+    public class ComplaintData : IComplaintData
+    {
+        private readonly IDbConnectionUtil _db;
+
+        public ComplaintData(IDbConnectionUtil db)
+        {
+            _db = db;
+        }
+
+        public Task<int> CreateComplaintAsync(ComplaintRequest complaint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteComplaintAsync(int ComplaintId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Complaint>> GetAllComplaintsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Complaint>> GetAllComplaintsByConsumerIdAsync(int consumerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Complaint?> GetComplaintByIdAsync(int complaintId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateComplaintAsync(int complaintId, ComplaintRequest complaint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
